ee6f2dadb3e8cbe517b64538e1d5e1533fe341ab
Raghu Betina <raghu@firstdraft.com>
2019-11-28 00:23:29 +0000

    Whitelist columns in CourseCategory for ActiveAdmin

--- app/admin/course_categories.rb
+++ app/admin/course_categories.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register CourseCategory do
+
+ permit_params :course_id, :category_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
