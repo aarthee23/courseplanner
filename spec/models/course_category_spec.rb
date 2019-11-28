ac7401564c3f08754b58abc39a6d2004f1655b38
Raghu Betina <raghu@firstdraft.com>
2019-11-28 00:22:34 +0000

    Direct association between courses and course_categories

--- spec/models/course_category_spec.rb
+++ spec/models/course_category_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe CourseCategory, type: :model do
   
     describe "Direct Associations" do
 
+    it { should belong_to(:course) }
+
     end
 
     describe "InDirect Associations" do
--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe Course, type: :model do
   
     describe "Direct Associations" do
 
+    it { should have_many(:course_categories) }
+
     end
 
     describe "InDirect Associations" do
